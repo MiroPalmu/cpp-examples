7dd22a3a7f7b9b4c6b12299171cd786d1cbb9884
Miro Palmu <email@miropalmu.cc>
2025-02-15 17:13:49 +0200

    proj: add sycl-clang

---
+++ proj/sycl-clang/syclutils.cpp
@@ -0,0 +1,36 @@
+#include "syclutils.hpp"
+
+#include <array>
+#include <print>
+
+#include "sycl/sycl.hpp"
+
+void
+syclutils::ls_platforms() {
+    // Loop through available platforms
+    for (auto const& platform : sycl::platform::get_platforms()) {
+        std::println("Found platform: {}", platform.get_info<sycl::info::platform::name>());
+        // Loop through available devices in this platform
+        for (auto const& device : platform.get_devices()) {
+            std::println("With device: ", device.get_info<sycl::info::device::name>());
+        }
+        std::println("");
+    }
+}
+
+void
+syclutils::print_kernel_indecies() {
+    constexpr int         size = 16;
+    std::array<int, size> data;
+    auto                  buff = sycl::buffer{ data };
+    std::println("Using sycl::cpu_selector_v because gpu backend will crash tonttu.");
+    auto q = sycl::queue{ sycl::cpu_selector_v };
+
+    q.submit([&](sycl::handler& h) {
+        auto acc = sycl::accessor{ buff, h };
+        h.parallel_for(size, [=](auto& idx) { acc[idx] = idx; });
+    });
+
+    for (const auto x : sycl::host_accessor{ buff }) { std::print("{} ", x); }
+    std::println("");
+}
